ae59aed65aca02b01227296d7bdecbc46c2a6191
Andrey <twinka4ik174@mail.ru>
2020-11-28 19:08:00 +0500

    1st, mb last

--- homework.py
+++ homework.py
@@ -7,28 +7,29 @@ from twilio.rest import Client
 
 def get_status(user_id):
     params = {
-        'user_ids':user_id,
-        'fields':'online',
-        'v':'5.92',
+        'user_ids': user_id,
+        'fields': 'online',
+        'v': '5.92',
         'access_token':os.getenv('VK_TOKEN')
     }
 
     response = requests.post(
         'https://api.vk.com/method/users.get', 
-        params=params)
-    return response.json()['response']['0']['online']  # Верните статус пользователя в ВК
+        params=params
+    )
+    return response.json()['response'][0]['online']  # Верните статус пользователя в ВК
 
 
-def sms_sender(sms_text, client):
+def sms_sender(sms_text):
     account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-    auth_token = og.getenv('TWILIO_AUTH_TOKEN')
+    auth_token = os.getenv('TWILIO_AUTH_TOKEN')
     client = Client(account_sid,auth_token)
     message = client.messages \
                 .create(
-                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_= os.getenv('NUMBER_FROM'),
-                     to= os.getenv('NUMBER_TO'),
-                 )
+                    body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                    from_= os.getenv('NUMBER_FROM'),
+                    to= os.getenv('NUMBER_TO'),
+                )
 
     return message.sid  # Верните sid отправленного сообщения из Twilio
 
